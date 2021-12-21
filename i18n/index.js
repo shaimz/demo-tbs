4189cba6c29ff259905b5c81a38736183f32fe8c
Cristian <cvicol2@gmail.com>
2021-12-21 21:15:03 +0200

    test

---
+++ i18n/index.js
@@ -0,0 +1,23 @@
+var banner = require('./locales/en/banner.json');
+var common = require('./locales/en/common.json');
+var form = require('./locales/en/form.json');
+var table = require('./locales/en/table.json');
+var widgets = require('./locales/en/widgets.json');
+
+const enTranslations = {
+  banner,
+  common,
+  form,
+  table,
+  widgets
+}
+
+const i18n = {
+	translations: {
+		en: enTranslations,
+		de: de.i18n,
+	},
+	defaultLang: 'en'
+}
+
+module.exports = i18n;
