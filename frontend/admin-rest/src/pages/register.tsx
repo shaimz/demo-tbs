c09e1bc52f1ec7ed706173807446a1e5b5c423c1
shaimz <cvicol2@gmail.com>
2021-12-22 15:20:37 +0200

    remove serverside translations

--- frontend/admin-rest/src/pages/register.tsx
+++ frontend/admin-rest/src/pages/register.tsx
@@ -7,11 +7,7 @@ import { useRouter } from "next/router";
 import { getAuthCredentials, isAuthenticated } from "@utils/auth-utils";
 import { ROUTES } from "@utils/routes";
 
-export const getStaticProps: GetStaticProps = async ({ locale }) => ({
-  props: {
-    ...(await serverSideTranslations(locale!, ["common", "form"])),
-  },
-});
+
 
 export default function RegisterPage() {
   const router = useRouter();
